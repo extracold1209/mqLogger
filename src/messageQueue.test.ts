671505bef648aed0d02963cb43a4d2aa42bb8a39
extracold1209 <extracold1209@gmail.com>
2020-03-23 17:50:50 +0900

    messageQueue 및 테스트코드 추가

---
+++ src/messageQueue.test.ts
@@ -0,0 +1,75 @@
+import chai, {assert} from 'chai'
+import chaiFs from 'chai-fs';
+import MessageQueue from './messageQueue';
+import fs from 'fs';
+
+chai.use(chaiFs);
+
+describe('MessageQueue', function() {
+    let messageQueue: MessageQueue;
+
+    beforeEach(function() {
+        messageQueue = new MessageQueue();
+    });
+
+    it('normal enqueue', function() {
+        messageQueue.queue({event: 'test', payload: 'dummy'});
+        assert.equal(messageQueue.length, 1);
+        assert.pathExists(messageQueue.path);
+    });
+
+    it('dequeue whole message',  function() {
+        const message = {event: 'test', payload: 'hello'};
+
+        messageQueue.queue(message);
+        const dequeueMessage = messageQueue.dequeue();
+
+        assert.deepEqual(message, dequeueMessage);
+        assert.pathExists(messageQueue.path);
+    });
+
+    it('dequeue message but message remained', function() {
+        const message = {event: 'test', payload: 'hello'};
+        const message2 = {event: 'test', payload: 'hello2'};
+
+        messageQueue.queue(message);
+        messageQueue.queue(message2);
+
+        const dequeueMessage = messageQueue.dequeue();
+
+        // 메시지는 선입선출
+        assert.deepEqual(message, dequeueMessage);
+
+        // 메세지가 남아있기때문에 add, delete 이벤트가 하나씩 있어야 한다
+        assert.pathExists(messageQueue.path);
+
+        // new line 데이터가 3줄 있어야함
+        assert.fileContentMatch(messageQueue.path, /(.*[\r\n]){3}/);
+    });
+
+    it('dequeue but empty queue', function() {
+        const message = messageQueue.dequeue();
+
+        assert.isUndefined(message);
+    });
+
+    it('load from file', async function() {
+        const newFilePath = './loadFile.txt';
+        const message = {event: 'test', payload: 'hello'};
+        const message2 = {event: 'test', payload: 'hello2'};
+        messageQueue.queue(message);
+        messageQueue.queue(message2);
+        messageQueue.dequeue();
+
+        fs.renameSync(messageQueue.path, newFilePath);
+        messageQueue.clear();
+
+        await messageQueue.initializeQueue(newFilePath);
+
+        assert.equal(messageQueue.length, 1);
+    });
+
+    afterEach(function() {
+        messageQueue.clear();
+    })
+});
---
+++ src/messageQueue.ts
@@ -0,0 +1,102 @@
+import fs from 'fs';
+
+const fsp = fs.promises;
+
+/**
+ * 메세지 큐가 하는 일
+ * 메세지를 넣으면 파일시스템에 정리한다. 물론 오브젝트로도 들고있는다.
+ * 그리고 큐에는 입력, 삭제 단위로 계속 append 한다.
+ * 오브젝트가 빈칸이 되면 파일을 삭제한다.
+ *
+ * 최초 실행시에는 파일시스템을 읽고 그에따라 오브젝트를 만든다.
+ * 그리고 파일을 삭제하고 add 로만 재구성한다.
+ *
+ */
+
+type MessageAction = {
+    action: 'add' | 'delete';
+    payload: { [key: string]: any }
+}
+
+
+type Message = { [key: string]: any };
+
+class MessageQueue {
+    private queueFilePath: string;
+    private queueObject: Message[] = [];
+
+    get length() {
+        return this.queueObject.length;
+    }
+
+    get path() {
+        return this.queueFilePath;
+    }
+
+    constructor(queueFilePath: string = './queue.txt') {
+        this.queueFilePath = queueFilePath;
+    }
+
+    queue(message: Message): void {
+        this.queueObject.push(message);
+        this.appendMessageToFile('add', message);
+    }
+
+    dequeue(): Message | undefined {
+        const message = this.queueObject.shift();
+        if (message) {
+            this.appendMessageToFile('delete', message);
+        }
+
+        return message;
+    }
+
+    async initializeQueue(newFilePath?: string) {
+        this.queueObject = [];
+
+        if (newFilePath) {
+            this.deleteQueueFile();
+            this.queueFilePath = newFilePath;
+        }
+
+        const fileBuffer = await fsp.readFile(this.queueFilePath);
+        const messages = fileBuffer.toString('utf8').split(/\r?\n/);
+
+        messages.forEach((msgString) => {
+            if (msgString === '') {
+                return;
+            }
+
+            const message = JSON.parse(msgString) as MessageAction;
+            if (message.action === 'add') {
+                this.queueObject.push(message.payload);
+            } else if (message.action === 'delete') {
+                this.queueObject.shift();
+            } else {
+                throw new Error('message from file : parsing error');
+            }
+        })
+
+        // 큐오브젝트를 초기화한다
+        // 파일을 한줄씩 읽는다
+        // add 이벤트면 큐오브젝트에 데이터를 추가
+        // delete 이벤트면 큐오브젝트에서 데이터를 제거
+    }
+
+    clear() {
+        this.queueObject = [];
+        this.deleteQueueFile();
+    }
+
+    private appendMessageToFile(action: 'add' | 'delete', payload: Message) {
+        fs.appendFileSync(this.queueFilePath, `${JSON.stringify({action, payload})}\r\n`);
+    }
+
+    private deleteQueueFile() {
+        if (fs.existsSync(this.queueFilePath)) {
+            fs.unlinkSync(this.queueFilePath);
+        }
+    }
+}
+
+export default MessageQueue;
